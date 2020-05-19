d0e7adf3595e1b2965cdaf320bc5974031e553ac
Keziah Odoi <59188702+or33@users.noreply.github.com>
2020-05-19 13:09:54 +0000

    Update LoginPageController.java

--- loginpage/LoginPageController.java
+++ loginpage/LoginPageController.java
@@ -149,6 +149,7 @@ public class LoginPageController implements Initializable {
                     }
                     catch(Exception e){
                     }
+                    e.printStackTrace();
                 }
                 
                 else 
